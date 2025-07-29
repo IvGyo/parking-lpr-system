57b48290f21c00363669d43982c0f217437bd751
IvGyo <ivozandy@gmail.com>
2025-07-29 11:06:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return 'Parking LPR system works!'
 
 if __name__ == '__main__':
     app.run()
